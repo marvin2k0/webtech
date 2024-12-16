7aabd5096cc05a88313ebf600c637de333e0ec36
Maurice <mauriceheimbach@gmail.com>
2024-12-16 20:23:08 +0100

    User profile page component created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {LandingPageComponent} from './components/landing-page/landing-page.compo
 import {NotFoundComponent} from './components/not-found/not-found.component';
 import {LoginPageComponent} from './components/login-page/login-page.component';
 import {SignupPageComponent} from './components/signup-page/signup-page.component';
+import {UserProfilePageComponent} from './components/user-profile-page/user-profile-page.component';
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'signin', component: LoginPageComponent},
   {path: 'signup', component: SignupPageComponent},
+  {path: 'userprofile', component: UserProfilePageComponent},
   {path: '**', component: NotFoundComponent}
 ];
