d4354e7f9b3dfe59b1a326700c5ed9ba5b144108
Marvin Leiers <marvin@leiers.org>
2024-12-16 20:01:07 +0100

    Modify landing page and other stuff

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {LandingPageComponent} from './components/landing-page/landing-page.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {SignupPageComponent} from './components/signup-page/signup-page.component';
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
+  {path: 'signin', component: LoginPageComponent},
+  {path: 'signup', component: SignupPageComponent},
   {path: '**', component: NotFoundComponent}
 ];
--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from '../navbar/navbar.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
