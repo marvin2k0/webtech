2254569c7a0477c94733198890b141a4cedb2f5b
Maurice <mauriceheimbach@gmail.com>
2024-12-17 22:07:49 +0100

    User profile page updated

--- frontend/src/app/components/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/components/user-profile-page/user-profile-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-user-profile-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    TranslatePipe
+  ],
   templateUrl: './user-profile-page.component.html',
   styleUrl: './user-profile-page.component.css'
 })
