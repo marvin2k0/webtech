abd0fdf97d84569647aa048cf35d6be276c2a087
Dean Radetzky <dean.radetzky@stud.uni-due.de>
2024-12-17 10:07:23 +0100

    Added Appointments to dashboard

---
+++ frontend/src/app/components/stepper-timeline/stepper-timeline.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stepper-timeline',
+  standalone: true,
+  imports: [],
+  templateUrl: './stepper-timeline.component.html',
+  styleUrl: './stepper-timeline.component.css'
+})
+export class StepperTimelineComponent {
+
+}
