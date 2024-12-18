56d0c6e7f03e11498b012558adfeae9e75fcbbda
Dean Radetzky <dean.radetzky@stud.uni-due.de>
2024-12-18 13:23:17 +0100

    Deutsche Übersetzungen auf landingpage

--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from '../navbar/navbar.component';
 import {RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    TranslatePipe
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
