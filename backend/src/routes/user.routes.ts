9d975b34eb743b7ff32b9feb13b833d50881d34d
Marvin Leiers <marvin@leiers.org>
2024-12-18 13:18:19 +0100

    Rename users api route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,6 +4,6 @@ import {createUser} from "../controller/user.controller";
 const router: Router = express.Router();
 
 // TODO router.get("/:username", getUserDetails);
-router.post("/create", createUser);
+router.post("/", createUser);
 
 export default router;
