45d90358b2645b4ae997e11e8b0aa3ab697e2e53
Marvin Leiers <marvin@leiers.org>
2024-12-22 18:57:58 +0100

    Started with login in Frontend

--- frontend/src/app/components/login-page/login-page.component.ts
+++ frontend/src/app/components/login-page/login-page.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  userService: UserService = inject(UserService)
+  username: string = ""
+  password: string = ""
 
+  onSubmit(): void {
+    this.userService.login(this.username, this.password).subscribe(response => {
+      if ("message" in response) {
+        console.error("Error occurred while trying to log in", response.message)
+      } else if ("data" in response) {
+        console.log(response.data.accessToken)
+
+        // TODO jwt speicher. mit httpOnly cookie Header aus be?
+        // - weiterleiten auf dashboard
+        // - login / register knöpfe ausblenden
+        // - etc.
+      }
+    })
+  }
 }
