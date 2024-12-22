2e517cc15cf9a6b618516814de267aebe09a9327
Maurice <mauriceheimbach@gmail.com>
2024-12-22 19:29:47 +0100

    Added Button click logic

--- frontend/src/app/components/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/components/user-profile-page/user-profile-page.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {TranslatePipe} from '@ngx-translate/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile-page',
   standalone: true,
   imports: [
     RouterLink,
-    TranslatePipe
+    TranslatePipe,
+    FormsModule
   ],
   templateUrl: './user-profile-page.component.html',
   styleUrl: './user-profile-page.component.css'
 })
 export class UserProfilePageComponent {
+  focusPointBool: boolean = true;
+  instituteBool: boolean = true;
+  dobBool: boolean = true;
 
+  onUsernameButtonClick(): void {}
+
+  onUserInformationEditButtonClick() {
+    this.focusPointBool = !this.focusPointBool;
+    this.instituteBool = !this.instituteBool;
+    this.dobBool = !this.dobBool;
+  }
 }
