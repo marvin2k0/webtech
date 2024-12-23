37770618ce1b58ef398e6aea3043f88ca47a224a
Marvin Leiers <marvin@leiers.org>
2024-12-23 11:41:18 +0100

    Add german translation

--- frontend/src/app/components/signup-page/signup-page.component.ts
+++ frontend/src/app/components/signup-page/signup-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    TranslatePipe
   ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
