74ae811cfd52b6b36b6ec59bc12472d38069e01d
Marvin Leiers <marvin@leiers.org>
2024-12-23 12:00:00 +0100

    Create angular reactive form + add call-to-action disabled style

--- frontend/src/app/components/signup-page/signup-page.component.ts
+++ frontend/src/app/components/signup-page/signup-page.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {TranslatePipe} from '@ngx-translate/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
   imports: [
     RouterLink,
-    TranslatePipe
+    TranslatePipe,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
 export class SignupPageComponent {
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', Validators.required),
+    passwordConfirm: new FormControl('', Validators.required),
+    acceptTos: new FormControl(false, Validators.required)
+  })
 
+  onSubmit() {
+    console.log(this.registerForm.value)
+  }
 }
