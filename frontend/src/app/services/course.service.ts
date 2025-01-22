2f28198f5a733170949dc990b99c3aceaabd5a01
Marvin Leiers <marvin@leiers.org>
2025-01-22 01:26:54 +0100

    Add basic cache management

--- frontend/src/app/services/course.service.ts
+++ frontend/src/app/services/course.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {IRestResponse} from '../model/http/rest-response';
 import {environment} from '../../environments/environment.development';
+import {CachingService} from './caching.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class CourseService {
   private baseUrl: string = `${environment.baseUrl}/course`
 
   http: HttpClient = inject(HttpClient)
+  cachingService: CachingService = inject(CachingService)
 
   findCourses(query: string): Observable<IRestResponse> {
     return this.http.get<IRestResponse>(`${this.baseUrl}?name=${query}&description=${query}`);
@@ -21,10 +23,12 @@ export class CourseService {
   }
 
   joinCourse(id: string): Observable<IRestResponse> {
+    this.cachingService.invalidateAll()
     return this.http.post<IRestResponse>(`${this.baseUrl}/join/${id}`, {})
   }
 
   leaveCourse(id: string ): Observable<IRestResponse> {
+    this.cachingService.invalidateAll()
     return this.http.post<IRestResponse>(`${this.baseUrl}/leave/${id}`, {})
   }
 }
