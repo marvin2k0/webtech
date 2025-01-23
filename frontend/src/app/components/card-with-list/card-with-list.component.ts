f7f5cfdc057f94f78f3c12b9bb88b8a8485f82e4
Dean Radetzky <dean.radetzky@stud.uni-due.de>
2025-01-23 20:32:16 +0100

    Dashboard component can now show enrolled courses

--- frontend/src/app/components/card-with-list/card-with-list.component.ts
+++ frontend/src/app/components/card-with-list/card-with-list.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {CardComponent} from '../card/card.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-card-with-list',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CardComponent,
+    NgForOf
   ],
   templateUrl: './card-with-list.component.html',
   styleUrl: './card-with-list.component.css'
 })
 export class CardWithListComponent {
+  @Input() objectArray: any;
+  @Input() heading!: string;
+  @Input() actionLink!: string;
+  @Input() hdLink!: string;
 
 }
--- frontend/src/app/components/dashboard-page/dashboard-page.component.ts
+++ frontend/src/app/components/dashboard-page/dashboard-page.component.ts
@@ -1,17 +1,48 @@
 import { Component } from '@angular/core';
 import {CardWithListComponent} from '../card-with-list/card-with-list.component';
 import {StepperTimelineComponent} from '../stepper-timeline/stepper-timeline.component';
+import {Router} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {CourseService} from '../../services/course.service';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
   imports: [
     CardWithListComponent,
-    StepperTimelineComponent
+    TranslatePipe
   ],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
 export class DashboardPageComponent {
 
+  courses: any[] = [];
+
+  constructor(private userService: UserService, private courseService: CourseService) { }
+
+  ngOnInit(): void {
+    this.getEnrolledCourses();
+  }
+
+  getEnrolledCourses() {
+    this.userService.getUserInformation().subscribe({
+      next: (res) => {
+        res.data.enrolledCourses.forEach((course: string) => { this.getCourseData(course) })
+      },
+      error: () => {
+
+      }
+    })
+  }
+
+  getCourseData(course: string) {
+    this.courseService.findCourseById(course).subscribe({
+      next: (res) => {
+        this.courses.push({ title: res.data.name!, subtitle: res.data.description.slice(0, 25), linkName: "→", link: "/course/" + course })
+      },
+      error: () => {}
+    })
+  }
 }
