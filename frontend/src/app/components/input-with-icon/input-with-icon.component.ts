a83b78dd0fdd9608bef5d548d2f9976a626442d5
Marvin Leiers <marvin@leiers.org>
2025-01-26 12:49:21 +0100

    Update and translate input placeholders

--- frontend/src/app/components/input-with-icon/input-with-icon.component.ts
+++ frontend/src/app/components/input-with-icon/input-with-icon.component.ts
@@ -1,17 +1,20 @@
 import {booleanAttribute, Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'input-with-icon',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    TranslatePipe
   ],
   templateUrl: './input-with-icon.component.html',
   styleUrl: './input-with-icon.component.css'
 })
 export class InputWithIconComponent {
   @Input({transform: booleanAttribute}) clearAfterSend: boolean = true
+  @Input() inputPlaceholder: string = "default_input_placeholder"
   @Output() clickFunction = new EventEmitter<string>();
   text: string = "";
 
